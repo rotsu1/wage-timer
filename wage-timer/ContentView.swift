98309fa12e80a28388423d192bdc4d37141fea0a
rotsu1 <ryuotsu123@gmail.com>
2025-12-15 17:10:14 +0900

    Initial Commit

---
+++ wage-timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wage-timer
+//
+//  Created by 乙津　龍　 on 15/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wage-timer/wage_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  wage_timerApp.swift
+//  wage-timer
+//
+//  Created by 乙津　龍　 on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct wage_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
